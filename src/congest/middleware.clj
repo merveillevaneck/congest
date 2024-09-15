168e2fa421d12f014b9b03fc65d7b06a346b4485
Merveille van Eck <merveillevaneck@gmail.com>
2024-09-15 15:23:17 +0200

    added some comments to middleware file

--- src/congest/middleware.clj
+++ src/congest/middleware.clj
@@ -2,6 +2,8 @@
   (:require [congest.schema :as is]
             [jsonista.core :as json]))
 
+;;  Will validate a request, parse the body if it exists
+;; and return it. If the body isnt present, returns nil.
 (defn parse-body [req]
   (try
     (if (and (is/request? req) (some? (:body req)))
@@ -9,9 +11,12 @@
       nil)
     (catch Exception _ nil)))
 
+;; just stringified the body, assuming all keys are keywords.
 (defn stringify [body]
   (json/write-value-as-string body json/keyword-keys-object-mapper))
 
+;; pre-stringifies the response of a handler by
+;; checking if it is a collection first
 (defn handle-response [response]
   (println "here")
   (let [resbody (:body response)
@@ -21,6 +26,10 @@
       (assoc response :body (stringify resbody))
       response)))
 
+;; Wraps a handler in parse-body and stringify.
+;; Processes the request body into a keywords-map.
+;; processes the response of handler into a an http
+;; readable respnse.
 (defn wrap [handler]
   (fn [req]
     (handle-response
